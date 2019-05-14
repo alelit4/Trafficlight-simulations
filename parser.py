f0fd1c6c070a4751e429d97b00384ee3d2233cc2
Ale <alelit4@gmail.com>
2019-05-14 13:11:49 +0100

    list of infringingVehicles added

--- parser.py
+++ parser.py
@@ -1,10 +1,13 @@
 import xml.etree.ElementTree as ET
 root = ET.parse('fulldata.xml').getroot()
 
+contViolations = 1
+infringingVehicles = []
 
 for step in root.iter('data'):
-   print("--> " + str(step.attrib["timestep"]))
+   #print("--> " + str(step.attrib["timestep"]))
    trafficlights = step.find('tls')
+   # Too see the state of the junction
    #for tl in trafficlights.iter('trafficlight'):
    #   print(" " + str(tl.attrib["state"]))
    trafficlightState = trafficlights[0].attrib["state"][0]  # rGrG state
@@ -14,13 +17,16 @@ for step in root.iter('data'):
    for vehicle in vehicles.iter('vehicle'):
       pos = float(vehicle.attrib["y"])
       id = vehicle.attrib["id"]
-      # and (pos > 500) and (pos < 518) ):
       if(trafficlightState == "r"):
          #print('--->> ROJO! ')
          if (pos > 500) and (pos < 520):
-            print("----->> pos < 500 and > 520 ! ")
+            print('Step %s ----->> TL Violation!' % step.attrib["timestep"] )
+            contViolations+=1
             print( str(id) + " -> pos = " + str(pos) )
+            if id not in infringingVehicles: 
+               infringingVehicles.append(id)
             #   print(" " + str(contVehicles) + ') y=' + pos + ' speed= ' + str(vehicle.attrib["speed"]))
       contVehicles += 1
    
-   
+print("There are " + str(len(infringingVehicles)) + " tlv")
+print(infringingVehicles)
