b768c87ed1cf43d8762337d4de2591e8dde7d26b
Ale <alelit4@gmail.com>
2019-05-14 13:56:21 +0100

    lil improvements in the clean of the code

--- parser.py
+++ parser.py
@@ -1,32 +1,34 @@
 import xml.etree.ElementTree as ET
 root = ET.parse('fulldata.xml').getroot()
 
-contViolations = 1
-infringingVehicles = []
+isDebugging = True # if it is debugging the program is verbose 
+infringingVehicles = [] # The list of all infractors 
+
+# The reference of limits is based on the location of the juction
+upperLimitJunction = 500 
+lowerLimitJunction = 520
 
 for step in root.iter('data'):
-   #print("--> " + str(step.attrib["timestep"]))
+
    trafficlights = step.find('tls')
    # Too see the state of the junction
    #for tl in trafficlights.iter('trafficlight'):
    #   print(" " + str(tl.attrib["state"]))
-   trafficlightState = trafficlights[0].attrib["state"][0]  # rGrG state
-   # print(" " + trafficlightState)
-   vehicles = step.find('vehicles')
-   contVehicles = 1
-   for vehicle in vehicles.iter('vehicle'):
-      pos = float(vehicle.attrib["y"])
-      id = vehicle.attrib["id"]
-      if(trafficlightState == "r"):
-         #print('--->> ROJO! ')
-         if (pos > 500) and (pos < 520):
-            print('Step %s ----->> TL Violation!' % step.attrib["timestep"] )
-            contViolations+=1
-            print( str(id) + " -> pos = " + str(pos) )
-            if id not in infringingVehicles: 
-               infringingVehicles.append(id)
-            #   print(" " + str(contVehicles) + ') y=' + pos + ' speed= ' + str(vehicle.attrib["speed"]))
-      contVehicles += 1
    
-print("There are " + str(len(infringingVehicles)) + "/1000 tlv")
-print(infringingVehicles)
+   trafficlightState = trafficlights[0].attrib["state"][0]  # rGrG state
+   allVehiclesInStep = step.find('vehicles')
+   vehiclesInStep = 1
+   for vehicle in allVehiclesInStep.iter('vehicle'):
+      locationY = float(vehicle.attrib["y"])
+      currentVehicleId = vehicle.attrib["id"]
+      if(trafficlightState == "r"):
+         if (locationY > upperLimitJunction) and (locationY < lowerLimitJunction):
+            if isDebugging:
+               print('Step %s ----->> Trafficlight Infraction!' % step.attrib["timestep"])
+               print( '%s -> pos = %i' % (currentVehicleId, locationY))
+            if currentVehicleId not in infringingVehicles:
+               infringingVehicles.append(currentVehicleId)
+      vehiclesInStep += 1
+   
+print('There are %i/1000 tlv' % len(infringingVehicles) )
+print(' '.join(infringingVehicles)) if isDebugging else 0
